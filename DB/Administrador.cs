9ac6f6e610e7bb28133ac600f1c5a2ce30f42021
adrian <josetrejos2000@gmail.com>
2021-03-18 20:52:42 -0600

    Propiedades, getters y setters, constructor de todas las clases

--- DB/Administrador.cs
+++ DB/Administrador.cs
@@ -2,6 +2,31 @@ namespace RestTEC.DB
 {
     public class Administrador
     {
-        
+        public string correo;
+        private string contrasena;
+        public Pedido[] Lista_Pedidos;
+
+        public Administrador(string correo, string contrasena, Pedido[] listaPedidos)
+        {
+            this.correo = correo;
+            this.contrasena = contrasena;
+            Lista_Pedidos = listaPedidos;
+        }
+        public string Correo
+        {
+            get => correo;
+            set => correo = value;
+        }
+        public string Contrasena
+        {
+            get => contrasena;
+            set => contrasena = value;
+        }
+
+        public Pedido[] ListaPedidos
+        {
+            get => Lista_Pedidos;
+            set => Lista_Pedidos = value;
+        }
     }
 }
--- DB/Menu.cs
+++ DB/Menu.cs
@@ -2,6 +2,15 @@ namespace RestTEC.DB
 {
     public class Menu
     {
-        
+        public Plato[] Lista_Platos;
+        public Menu(Plato[] listaPlatos)
+        {
+            Lista_Platos = listaPlatos;
+        }
+        public Plato[] ListaPlatos
+        {
+            get => Lista_Platos;
+            set => Lista_Platos = value;
+        }
     }
 }
